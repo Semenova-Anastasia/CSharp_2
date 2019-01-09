93dd9dcb491a4734389ae826850307bab210e472
Semenova-Anastasia <haterg@yandex.ru>
2019-01-09 12:21:24 +0300

    класс наряда, исключение, интерфейс для столкновений

---
+++ MyGame/MyGame/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MyGame
+{
+    /// <summary>
+    /// Класс снарядов.
+    /// </summary>
+    class Bullet : BaseObject
+    {
+        public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawRectangle(Pens.OrangeRed, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X + 10;
+            if (Pos.X > Game.Width)
+            {
+                Pos.X = 15;
+            }
+        }
+        
+        public override void Regenerate()
+        {
+            base.Regenerate();
+            Pos.X = 15;
+        }
+    }
+}
